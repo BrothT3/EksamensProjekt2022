f36d3e454ae0c06aa65124a6942f3c7049c02076
DanielKjr <93762921+DanielKjr@users.noreply.github.com>
2022-05-09 18:25:43 +0200

    Moving camera and debug tool

---
+++ DebugTool.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace EksamensProjekt2022
+{
+    public class DebugTool
+    {
+        private SpriteFont font;
+
+        public DebugTool()
+        {
+            font = GameWorld.Instance.Content.Load<SpriteFont>("Font");
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Player player = (Player)GameWorld.Instance.FindObjectOfType<Player>();
+            int x = (int)player.GameObject.Transform.Position.X;
+            int y = (int)player.GameObject.Transform.Position.Y;
+
+          
+            spriteBatch.DrawString(font,$" ({x.ToString()} , {y.ToString()})", 
+                new Vector2(player.GameObject.Transform.Position.X -40, player.GameObject.Transform.Position.Y + 50), Color.White);
+         
+
+        }
+    }
+}
