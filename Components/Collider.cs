26e31fe47feec4ba9bef0c4b1f4149d049491ae1
DanielKjr <93762921+DanielKjr@users.noreply.github.com>
2022-05-10 11:58:59 +0200

    collider component

---
+++ Components/Collider.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace EksamensProjekt2022
+{
+    public class Collider : Component
+    {
+        private SpriteRenderer _spriteRenderer;
+        private Texture2D texture;
+
+        public Rectangle CollisionBox
+        {
+            get
+            {
+                return new Rectangle((int)GameObject.Transform.Position.X,
+                    (int)GameObject.Transform.Position.Y,
+                    _spriteRenderer.Sprite.Width,
+                    _spriteRenderer.Sprite.Height );
+            }
+        }
+
+
+        public override void Start()
+        {
+            _spriteRenderer = (SpriteRenderer)GameObject.GetComponent<SpriteRenderer>();
+            texture = GameWorld.Instance.Content.Load<Texture2D>("Pixel");
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+#if DEBUG
+            DrawRectangle(CollisionBox, spriteBatch);
+#endif
+        }
+
+
+        public void DrawRectangle(Rectangle collisionBox, SpriteBatch spriteBatch)
+        {
+            Rectangle topLine = new Rectangle(collisionBox.X, collisionBox.Y, collisionBox.Width, 1);
+            Rectangle bottomLine = new Rectangle(collisionBox.X, collisionBox.Y + collisionBox.Height, collisionBox.Width, 1);
+            Rectangle rightLine = new Rectangle(collisionBox.X + collisionBox.Width, collisionBox.Y, 1, collisionBox.Height);
+            Rectangle leftLine = new Rectangle(collisionBox.X, collisionBox.Y, 1, collisionBox.Height);
+
+            spriteBatch.Draw(texture, topLine, null, Color.Red, 0, Vector2.Zero, SpriteEffects.None, 1);
+            spriteBatch.Draw(texture, bottomLine, null, Color.Red, 0, Vector2.Zero, SpriteEffects.None, 1);
+            spriteBatch.Draw(texture, rightLine, null, Color.Red, 0, Vector2.Zero, SpriteEffects.None, 1);
+            spriteBatch.Draw(texture, leftLine, null, Color.Red, 0, Vector2.Zero, SpriteEffects.None, 1);
+        }
+
+
+        private void CheckCollision()
+        {
+            foreach (Collider other in GameWorld.Instance.Colliders)
+            {
+                if (other != this && other.CollisionBox.Intersects(CollisionBox))
+                {
+                    //do collision thing
+                }
+            }
+        }
+    }
+}
