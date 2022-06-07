4a9c9e273c89e18e0f958591f856f218963915ca
BrothT3 <79831328+BrothT3@users.noreply.github.com>
2022-06-07 11:50:16 +0200

    oprydning

--- GameControl/GameStates/Playing.cs
+++ GameControl/GameStates/Playing.cs
@@ -10,7 +10,7 @@ namespace EksamensProjekt2022
     public class Playing : GameState
     {
         public UserInterface userInterface;
-        public AreaManager areaManager;
+        public AreaManager areaManager = new AreaManager();
         public TimeManager timeManager;
         public DebugTool _debugTools;
         #region Lists
@@ -37,7 +37,7 @@ namespace EksamensProjekt2022
         public MapManager _mapManager;
         private Vector2 buttonOffset;
         public Cell selectedCell;
-
+        
         public Playing()
         {
             CellCount = 40;
